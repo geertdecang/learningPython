f61e0e282a574f5469bb38fd01f6872ad6e75a29
geertdecang <35036055+geertdecang@users.noreply.github.com>
2024-07-07 23:36:42 +0200

    variables

--- hello.py
+++ hello.py
@@ -1,3 +1,9 @@
 # Dit is een comment
 """ En dit is eentje die over meerdere
 lijnen loopt. """
+# This variable contains an integer
+quantity=10
+# This variable contains a float
+unit_price=1.99
+extended_price = unit_price * quantity
+print(extended_price)
